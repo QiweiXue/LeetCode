e8581fe31bc5c262e278eacb1f26f4a4a1e7bf5a
Qiwei Xue <xueqiweiyi@gmail.com>
2017-06-24 00:35:02 +0800

    finish ex11

--- Ex11_ContainerWithMostWater/ContainerWithMostWater.h
+++ Ex11_ContainerWithMostWater/ContainerWithMostWater.h
@@ -1,11 +1,23 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 
 class ContainerWithMostWater
 {
 public:
 	int maxArea(std::vector<int>& height) {
-
+		int left = 0;
+		int right = height.size() -1 ;
+		int area = right * std::min(height[0], height[right]);
+		while (left < right)
+		{
+			area = std::max(area, std::min(height[left], height[right]) *(right-left));
+			if (height[left] < height[right])
+				left++;
+			else
+				right--;
+		}
+		return area;
 	}
 };
 
--- Ex11_ContainerWithMostWater/main.cpp
+++ Ex11_ContainerWithMostWater/main.cpp
@@ -5,5 +5,8 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
+	vector<int> height{ 4,6,2,6,7,11,2 };
+	std::cout << ContainerWithMostWater().maxArea(height);
+	system("pause");
 	return 0;
 }
